7b240df61c14d9aa12a1676b8c9d8aa45ba31028
ha-yun <deu05230@naver.com>
2022-04-07 22:54:58 +0900

    03

---
+++ js/diary.js
@@ -0,0 +1,35 @@
+$(function(){
+    var CreateModalDiv = $("#create_modal");
+    var createDiaryBtn = $("#create");
+    var closeDiaryBtn = $("#create_modal").children('button');
+
+    var DiaryBtn = $("#diary").children('main').children('section').children('button');
+    var DiaryBox = $("#diary_box");
+    var closeDiaryBox = $("#diary_box").children('button');
+    Eventlist();
+
+    function OpenDiary(){
+        DiaryBox.fadeToggle();
+        DiaryBox.children('div').removeClass();
+        DiaryBox.children('div').addClass($(this).attr('id'));
+        DiaryBox.children('span').text($(this).children('span').text());
+        DiaryBox.children('p').text($(this).children('p').text());
+    }
+    function CloseDiary(){
+        DiaryBox.fadeToggle();
+    }
+
+    function CreateModal(){
+        CreateModalDiv.fadeIn();
+    }
+    function CloseModal(){
+        CreateModalDiv.fadeOut();
+    }
+
+    function Eventlist(){
+        createDiaryBtn.on('click', CreateModal);
+        closeDiaryBtn.on('click', CloseModal);
+        closeDiaryBox.on('click', CloseDiary);
+        DiaryBtn.on('click', OpenDiary);
+    }
+})
---
+++ js/music.js
@@ -0,0 +1,48 @@
+$(function(){
+    var prevBtn = $("#prev");
+    var nextBtn = $("#next");
+    var MusicDiv = $("#music").children('main').children('section').children('div');
+    var MusicMain = $("#music").children('main');
+
+    EventList();
+
+    function ShowMusic(){
+        MusicDiv.removeClass();
+        $(this).addClass("main_music");
+    }
+
+    function NextList(){
+        let MusicMainWidth =  parseInt(MusicMain.width());
+        let MusicSection = parseInt($("#music").children('main').children('section').width());
+        let MusicMainX = MusicMain.css('transform').split(',')[4];
+        
+        if( MusicMainX == 0){
+            MusicMain.css({'transform':'translate('+ -MusicSection+'px,'+0+')'})
+        }else if(MusicMainX < 0 && MusicMainX == -(MusicMainWidth-MusicSection*2)){
+            MusicMainX = parseFloat(MusicMainX) - parseFloat(MusicSection);
+            MusicMain.css({'transform':'translate('+ MusicMainX+'px,'+0+')'})
+        }else if(MusicMainX < 0 && MusicMainX == -(MusicMainWidth-MusicSection)){
+            MusicMain.css({'transform':'translate('+ 0+','+0+')'})}
+    }
+
+    function PrevList(){
+        let MusicMainWidth =  parseInt(MusicMain.width());
+        let MusicSection = parseInt($("#music").children('main').children('section').width());
+        let MusicMainX = MusicMain.css('transform').split(',')[4];
+
+        if(MusicMainX == 0){
+            MusicMain.css({'transform':'translate('+ -(MusicMainWidth-MusicSection)+'px,'+0+')'});
+        }else if(MusicMainX < 0 && MusicMainX == -(MusicMainWidth-MusicSection)){
+            MusicMainX = parseInt(MusicMainX) + parseInt(MusicSection);
+            MusicMain.css({'transform':'translate('+ MusicMainX+'px,'+0+')'})
+        }else if(MusicMainX < 0 && MusicMainX == -(MusicMainWidth-MusicSection*2)){
+            MusicMain.css({'transform':'translate('+ 0+','+0+')'})}
+    }
+
+
+    function EventList(){
+        MusicDiv.on('click', ShowMusic);
+        nextBtn.on('click', NextList);
+        prevBtn.on('click', PrevList);
+    }
+})
